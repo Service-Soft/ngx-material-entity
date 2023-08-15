0af25e4c4085dd45af66a07f64a437b0f5d3a229
tim-fabian <fabian@service-soft.de>
2023-08-15 15:46:00 +0200

    fixed eslint

--- projects/ngx-material-entity/src/encapsulation/uuid.utilities.ts
+++ projects/ngx-material-entity/src/encapsulation/uuid.utilities.ts
@@ -1,11 +1,13 @@
 import * as uuid from 'uuid';
 
 /**
- *
+ * Encapsulates functionality of the uuid package.
  */
 export abstract class UUIDUtilities {
     /**
+     * Creates a new v4 uuid.
      *
+     * @returns The created uuid.
      */
     static create(): string {
         return uuid.v4();
