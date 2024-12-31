2a2e56a73f8b30f5eca3eeba207fb84697e83929
Tim Fabian <fabian@service-soft.de>
2024-12-31 03:22:47 +0100

    hotfix #1

--- projects/ngx-material-entity/src/components/table/edit-dialog/edit-entity-dialog.component.ts
+++ projects/ngx-material-entity/src/components/table/edit-dialog/edit-entity-dialog.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, EnvironmentInjector, EventEmitter, Inject, OnInit, Output, runInInjectionContext } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
@@ -39,11 +40,11 @@ import { TooltipComponent } from '../../tooltip/tooltip.component';
         CommonModule,
         MatDialogModule,
         MatButtonModule,
-        NgxMatEntityConfirmDialogComponent,
         MatMenuModule,
         MatBadgeModule,
         TooltipComponent,
-        NgxMatEntityFormComponent
+        NgxMatEntityFormComponent,
+        FormsModule
     ]
 })
 export class NgxMatEntityEditDialogComponent<EntityType extends BaseEntityType<EntityType>> implements OnInit {
